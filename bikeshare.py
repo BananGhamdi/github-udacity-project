f7d69ce073613cd166f4529c86fc8bc68012f7ca
Sary <Abdulaziz>
2021-03-11 22:27:30 +0300

    update README and bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -1,10 +1,8 @@
 import time
+# install these libraries first using pip
 import pandas as pd
 import numpy as np
 
-
-
-# Master
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
